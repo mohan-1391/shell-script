2a14c9b7e718f9708e000cb2413c0c436dbe7b98
Mohan Satya Pavan Borra (Accenture International Limite) <v-mborra@microsoft.com>
2025-01-06 21:54:33 -0800

    array error

--- 5-arraylist.sh
+++ 5-arraylist.sh
@@ -2,7 +2,7 @@
 
 MOVIES=("rrr""puspa""sahoo")
 
-echo "first movie ; $MOVIES[0]"
-echo "SECOND movie ; $MOVIES[1]"
-echo "THIRD movie ; $MOVIES[2]"
-echo "ALL movie ; $MOVIES[@]"
+echo "first movie ; ${MOVIES[0]}"
+echo "SECOND movie ; ${MOVIES[1]}"
+echo "THIRD movie ; ${MOVIES[2]}"
+echo "ALL movie ; ${MOVIES[@]}"
