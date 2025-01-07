24661ca10402df63f7c9c3102e8e549969de2688
Mohan Satya Pavan Borra (Accenture International Limite) <v-mborra@microsoft.com>
2025-01-07 01:18:15 -0800

    array error

---
+++ installmysql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=(id -u)
+
+if ( $userid -ne 0 )
+then 
+     echo "Error : user must be root user"
+ exit 1     
+fi
+dnf install list mysql
+if ( $? -ne 0 )
+then
+    dnf install mysql -y
+    if ( $? ne 0 )
+    then
+        echo "my sql instalation failed"
+        exit 1
+    else 
+        echo "my sql installed successfully"
+else 
+    echo "my sql already installed"
+    fi            
