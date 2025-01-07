ec1a3a8f158bc03612258bc43b8b180b7f6cfc7c
Mohan Satya Pavan Borra (Accenture International Limite) <v-mborra@microsoft.com>
2025-01-06 22:34:22 -0800

    array error

--- 6-conditions.sh
+++ 6-conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [$NUMBER -gt 100]
 then
-echo "number is greaterthan 100"
+    echo "number is greaterthan 100"
 else
-echo "number is lessthan 100"
+    echo "number is lessthan 100"
 fi
