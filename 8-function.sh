5846c3e3aa7a5fee78532ea2d50527fde07e87c2
Mohan Satya Pavan Borra (Accenture International Limite) <v-mborra@microsoft.com>
2025-01-07 23:28:54 -0800

    function

--- 8-function.sh
+++ 8-function.sh
@@ -2,14 +2,8 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then
-    echo "ERROR:: You must have sudo access to execute this script"
-    exit 1 #other than 0
-fi
 
-VALIDATE=(){ 
-
+VALIDATE(){ 
     if [ $1 -ne 0 ]
     then
         echo "$2 ... FAILURE"
@@ -19,7 +13,11 @@ VALIDATE=(){
     fi
 }
 
-
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
 
 dnf list installed mysql
 
